d0f2beb41386db3cc279654f29b7f38267485018
Mike Watson <watsonmikej92@gmail.com>
2022-11-30 19:12:15 +0900

    Initial Commit

---
+++ GraphTheory/GraphTheoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphTheoryApp.swift
+//  GraphTheory
+//
+//  Created by ワトソン・マイク on 2022/11/30.
+//
+
+import SwiftUI
+
+@main
+struct GraphTheoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
